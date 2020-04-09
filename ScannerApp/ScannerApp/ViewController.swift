c70112564515cd2c6162c7e0658498b9f3cf8a2a
Raul Flores <raulflores650@gmail.com>
2020-04-09 16:10:55 -0700

    UI updated

---
+++ ScannerApp/ScannerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScannerApp
+//
+//  Created by Raul Flores on 4/8/20.
+//  Copyright © 2020 LosTresAmigos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
